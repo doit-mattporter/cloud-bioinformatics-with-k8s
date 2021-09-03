c8a814a7c0f7adc9650a485d7ce633397bc66c26
doit-mattporter <matt.porter@doit-intl.com>
2021-09-02 21:22:08 -0700

    Added logging roles to GKE Standard

--- GCP/terraform/standard/gke_iam.tf
+++ GCP/terraform/standard/gke_iam.tf
@@ -17,3 +17,27 @@ resource "google_project_iam_member" "associate-gcs-obj-admin" {
   role    = "roles/storage.objectAdmin"
   member  = "serviceAccount:${google_service_account.gke_cluster_bioinformatics_sa.email}"
 }
+
+resource "google_project_iam_member" "associate-log-writer" {
+  project = var.gcp_project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.gke_cluster_bioinformatics_sa.email}"
+}
+
+resource "google_project_iam_member" "associate-metric-writer" {
+  project = var.gcp_project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.gke_cluster_bioinformatics_sa.email}"
+}
+
+resource "google_project_iam_member" "associate-monitoring-viewer" {
+  project = var.gcp_project_id
+  role    = "roles/monitoring.viewer"
+  member  = "serviceAccount:${google_service_account.gke_cluster_bioinformatics_sa.email}"
+}
+
+resource "google_project_iam_member" "associate-stackdriver-metadata-writer" {
+  project = var.gcp_project_id
+  role    = "roles/stackdriver.resourceMetadata.writer"
+  member  = "serviceAccount:${google_service_account.gke_cluster_bioinformatics_sa.email}"
+}
